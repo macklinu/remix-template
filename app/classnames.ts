e2f57ee1101de45bc16c000592a8797d9d131f48
mackie <macklinu@users.noreply.github.com>
2023-01-27 13:46:10 -0500

    Add Tailwind classnames helper

---
+++ app/classnames.ts
@@ -0,0 +1,7 @@
+import type { ClassValue } from 'clsx'
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function classnames(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
