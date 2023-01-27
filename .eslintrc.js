ca7df72fd5affc9ebf7cb00f3cd3d6508cab74f5
mackie <macklinu@users.noreply.github.com>
2023-01-27 12:15:09 -0500

    Set up ESLint import order and sorting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,26 @@ module.exports = {
     '@remix-run/eslint-config/node',
     'plugin:tailwindcss/recommended',
   ],
+  plugins: ['no-relative-import-paths'],
+  rules: {
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'never',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'no-relative-import-paths/no-relative-import-paths': [
+      'error',
+      { allowSameFolder: false, rootDir: 'app', prefix: '~' },
+    ],
+  },
 }
